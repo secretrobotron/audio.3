1933a752b9a0bee3339b6839a84f33c343b8e5e0
secretrobotron <secretrobotron@gmail.com>
2011-05-18 00:52:33 -0400

    Added corban & humph with effects.

--- js/segment2.js
+++ js/segment2.js
@@ -6,6 +6,10 @@ SegmentList.addSegment(function () {
   var popcorn;
   var scene, animkit, bf3d;
 
+  var cameraTarget = [0,0,0],
+      cameraMode = 0,
+      cameraPosition = [0,0,0];
+
   function makeWireBoxObject(boxObject,boxSize,boxMesh) {
     for (var i = 1; i <= boxSize; i++) {
       boxObject.bindChild(new CubicVR.SceneObject({position:[-boxSize/2+i,boxSize/2,boxSize/2],mesh:boxMesh}));
@@ -73,6 +77,47 @@ SegmentList.addSegment(function () {
       animkit.transition(START_TIME+28, 50, 2, boxObject, "spiral","out");
       animkit.transition(START_TIME+48, 40, 2, boxObject, "explode");
 
+
+      popcorn.code({
+        start: START_TIME+21,
+        end: START_TIME+43,
+        onStart: function (options) {
+          var bfStr = bf3d.genString('humph');
+          bfStr.position = [-3, 3, -8];
+          var startTime = START_TIME + 20;
+          var mot = bfStr.motion = new CubicVR.Motion();
+          mot.setKey(CubicVR.enums.motion.POS, CubicVR.enums.motion.X, startTime, bfStr.position[0]);
+          mot.setKey(CubicVR.enums.motion.POS, CubicVR.enums.motion.X, startTime+3, bfStr.position[0]+2);
+          mot.setBehavior(CubicVR.enums.motion.ROT, CubicVR.enums.motion.Y, CubicVR.enums.envelope.behavior.CONSTANT, CubicVR.enums.envelope.behavior.CONSTANT);
+          options.bfStr = bfStr;
+          animkit.transition(START_TIME+40, 40, 2, bfStr, "explode", 'out');
+          scene.bindSceneObject(bfStr);
+        },
+        onEnd: function (options) {
+          scene.removeSceneObject(options.bfStr);
+        },
+      });
+
+      popcorn.code({
+        start: START_TIME+21,
+        end: START_TIME+43,
+        onStart: function (options) {
+          var bfStr = bf3d.genString('& corban');
+          bfStr.position = [2, 2, -8];
+          var startTime = START_TIME + 20;
+          var mot = bfStr.motion = new CubicVR.Motion();
+          mot.setKey(CubicVR.enums.motion.POS, CubicVR.enums.motion.X, startTime, bfStr.position[0]);
+          mot.setKey(CubicVR.enums.motion.POS, CubicVR.enums.motion.X, startTime+3, bfStr.position[0]+2);
+          mot.setBehavior(CubicVR.enums.motion.ROT, CubicVR.enums.motion.Y, CubicVR.enums.envelope.behavior.CONSTANT, CubicVR.enums.envelope.behavior.CONSTANT);
+          options.bfStr = bfStr;
+          animkit.transition(START_TIME+41, 40, 2, bfStr, "explode", 'out');
+          scene.bindSceneObject(bfStr);
+        },
+        onEnd: function (options) {
+          scene.removeSceneObject(options.bfStr);
+        },
+      });
+
     },
     load: function () {
       scene.bindSceneObject(boxObject);
@@ -84,6 +129,9 @@ SegmentList.addSegment(function () {
       var seconds = timer.getSeconds();
       scene.camera.position[0] = 5 * Math.sin(seconds / 5) + Math.cos(seconds / 2) * 3.5;
       scene.camera.position[2] = 5 * Math.cos(seconds / 5) + Math.cos(seconds / 2) * 3.5;
+      //scene.camera.target[0] = (scene.camera.target[0] - cameraTarget[0]) *.85;
+      //scene.camera.target[1] = (scene.camera.target[1] - cameraTarget[1]) *.85;
+      //scene.camera.target[2] = (scene.camera.target[2] - cameraTarget[2]) *.85;
     },
   });
 
